bf525d96f1398c7b40d330684f7492b517fc304a
Your Name <khuranaayush08@gmail.com>
2024-04-03 23:47:13 +0530

    added some routes

--- backend/Route/routes.js
+++ backend/Route/routes.js
@@ -1,4 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const {signup,login,updateProfile,getByName} = require("../controllers/Auth");
+const {getBalance,transfer} = require("../controllers/Account");
+const {auth} = require("../middleware/Auth");
+
+router.post("/login",login);
+router.post("/signup",signup);
+router.post("/updateProfile",auth,updateProfile);
+router.get("/get-by-string",getByName)
+
+router.get("/get-balance",auth,getBalance);
+router.put("/transfer",auth,transfer);
 
 module.exports = router;
